0d0410162cbbb7dd3c5b5b07bac20130741e5642
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2025-01-13 13:13:26 +0100

    CORS

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const cors = require('cors')
+
 const gamesRoutes = require('./routes/gamesRoutes');
 
 const app = express();
 
 const PORT = 3000;
 const RUTA = '/';
+const WEB = 'http://localhost:5173/';
 
 
 // Middleware para parsear JSON
+app.use(cors({
+  origin: WEB, // URL de tu aplicación Vue (ajusta el puerto según corresponda)
+  credentials: true // Si necesitas enviar cookies
+})) 
+
 app.use(express.json());
 
 // Rutas
